0f166831eb2e588c0d76ad6acafe03e96bcdfefa
chenmingqin <1450291172@qq.com>
2019-04-22 13:42:58 +0800

    netty配置

--- src/main/java/com/netty/test/common/RemotingHelper.java
+++ src/main/java/com/netty/test/common/RemotingHelper.java
@@ -24,4 +24,15 @@ public class RemotingHelper {
 
         return "";
     }
+
+    public static String parseSocketAddressAddr(SocketAddress socketAddress) {
+        if (socketAddress != null) {
+            final String addr = socketAddress.toString();
+
+            if (addr.length() > 0) {
+                return addr.substring(1);
+            }
+        }
+        return "";
+    }
 }
